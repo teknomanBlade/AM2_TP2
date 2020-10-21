7a59af02a28479ff7ae51e5964e44e997251f078
Marcelo Luna <luna.marceloandres@gmail.com>
2020-10-20 22:06:06 -0300

    Proyecto Inicial
    Funcionalidad de muestra de IA encontrada

---
+++ TP2_AM2/Assets/Scripts/NodesGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodesGenerator : MonoBehaviour
+{
+    public static bool IsFirst = true;
+    public static int Counter = 0;
+    public List<Node> grid = new List<Node>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var item in grid)
+        {
+            var value = Random.Range(0, 7);
+            if (value > 3)
+            {
+                if (item.name.Equals("Node") || item.name.Equals("Node (109)"))
+                {
+                    item.isBlocked = false;
+                }
+                else {
+                    item.isBlocked = true;
+                }
+                
+            }
+            else {
+                item.isBlocked = false;
+            }
+        }
+        StartCoroutine(DrawNeighbours());
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator DrawNeighbours()
+    {
+        yield return new WaitForSeconds(0.2f);
+        Debug.Log("My Grid Length " + grid.Count);
+        foreach (var e in grid)
+        {
+            e.CheckNeighbors();
+        }
+    }
+}
