036abc7d153a3b47d0e1fd9052cf9f1cf45d4f38
NicolasTheHuman <56463262+NicolasTheHuman@users.noreply.github.com>
2020-11-09 23:50:44 -0300

    Borro los nodos que no se usan
    Ditto

--- TP2_AM2/Assets/Scripts/GridGenerator.cs
+++ TP2_AM2/Assets/Scripts/GridGenerator.cs
@@ -9,25 +9,19 @@ public class GridGenerator : MonoBehaviour
 	public int width; //cantidad de nodos por ancho
 	public int height; //cantidad de nodos por alto
 	public float offset; //la distancia entre los nodos
-    public Vector3 InitialPos; //posición inicial de la grilla, Nico fijate de usarla
+	public Vector3 InitialPos; //posición inicial de la grilla, Nico fijate de usarla
 
 	private Transform _container; //acá guardo los nodos que genero para que no sea un lío la jerarquia 
 	private List<Node> _myNodes = new List<Node>(); //lista de todos los nodos generados
-
-	// Start is called before the first frame update
-	void Start()
-	{
-		//GenerateGrid();
-	}
 
 	//Crea un los nodos adentro de un contenedor, según el width, height y offset
 	public void GenerateGrid()
 	{
 		if (_container != null) return;
 
-        _myNodes.Clear();
-        _container = new GameObject().transform;
-        _container.transform.gameObject.AddComponent<NodesGenerator>();
+		_myNodes.Clear();
+		_container = new GameObject().transform;
+		_container.transform.gameObject.AddComponent<NodesGenerator>();
 		_container.position = Vector3.zero;
 		_container.transform.name = "AllNodes";
 
@@ -39,7 +33,7 @@ public class GridGenerator : MonoBehaviour
 				var tempNode = Instantiate(sphere, new Vector3(j * offset, 0, i * offset), Quaternion.identity);
 				tempNode.transform.SetParent(_container);
 				tempNode.transform.name = "Node " + counter;
-                tempNode.NodeID = counter;
+				tempNode.NodeID = counter;
 				tempNode.radius = offset;
 				_myNodes.Add(tempNode);
 
@@ -47,10 +41,10 @@ public class GridGenerator : MonoBehaviour
 
 			}
 		}
-        var nodesGenerator = _container.gameObject.GetComponent<NodesGenerator>();
-        nodesGenerator.grid = _myNodes;
+		var nodesGenerator = _container.gameObject.GetComponent<NodesGenerator>();
+		nodesGenerator.grid = _myNodes;
 
-        Debug.Log("I created the grid");
+		Debug.Log("I created the grid");
 
 		StartCoroutine(CheckNode());
 	}
@@ -58,19 +52,24 @@ public class GridGenerator : MonoBehaviour
 	//Necesito la corrutina para que no se llamen las funciones en el mismo frame
 	IEnumerator CheckNode()
 	{
+		List<Node> nodeToRemove = new List<Node>();
 		yield return new WaitForSeconds(0.01f);
-		CheckAndDeactivate();
-	}
-
-	//Desactiva los nodos que considera que estan fuera del nivel
-	void CheckAndDeactivate()
-	{
 		for (int i = 0; i < _myNodes.Count; i++)
 		{
 			var tempNode = _myNodes[i].NodesToDeactivate();
 			if (tempNode != null)
-				tempNode.gameObject.SetActive(false);
-			_myNodes[i].CheckNeighbors();
+			{
+				nodeToRemove.Add(tempNode);
+				DestroyImmediate(tempNode.gameObject);
+			}
+		}
+
+		for (int i = 0; i < nodeToRemove.Count; i++)
+		{
+			if (_myNodes.Contains(nodeToRemove[i]))
+			{
+				_myNodes.Remove(nodeToRemove[i]);
+			}
 		}
 	}
 }
